964afb7999a931d3261116ec7e95dd5cda01bbfe
rcmdnk <rcmdnk@gmail.com>
2013-02-16 01:43:07 +0100

    initial import

---
+++ .pythonstartup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os, readline
+histfile = os.path.join(os.environ["HOME"], ".pyhist")
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+import atexit
+atexit.register(readline.write_history_file, histfile)
+del os, histfile
