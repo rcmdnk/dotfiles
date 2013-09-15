b16ec7f36fa2359f334852f2d5ffbba7b74d55a7
rcmdnk <rcmdnk@gmail.com>
2013-09-15 03:22:07 +0200

    .mac.sh, from darwin11

--- .mac.sh
+++ .mac.sh
@@ -1,1 +1,6 @@
 #alias ctags='/Applications/MacVim.app/Contents/MacOS/ctags "$@"' # not in the recent MacVim
+
+# ssh agent
+if [ "$SSH_AUTH_SOCK" = "" ];then
+  export SSH_AUTH_SOCK=`/usr/sbin/lsof|grep ssh-agent|grep Listeners|awk '{print $8}'`
+fi
