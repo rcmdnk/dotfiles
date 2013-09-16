6e0bc81c4508e30eb85be1924026131c5f44fb8c
rcmdnk <rcmdnk@gmail.com>
2013-09-16 03:22:12 +0200

    .mac.sh .vimrc Use (vim
    vimdiff
    
    diff
    :diffoff
    diff
    :diffupdate
    .vimrc.not_used
    (vim
    
    vimdiff
    
    diff
    :diffoff
    diff
    :diffupdate, from darwin11

--- .mac.sh
+++ .mac.sh
@@ -2,5 +2,15 @@
 
 # ssh agent
 if [ "$SSH_AUTH_SOCK" = "" ];then
-  export SSH_AUTH_SOCK=`/usr/sbin/lsof|grep ssh-agent|grep Listeners|awk '{print $8}'`
+  #export SSH_AUTH_SOCK=`/usr/sbin/lsof|grep ssh-agent|grep Listeners|awk '{print $8}'`
+  sock_tmp=(`ls -t /tmp/launch*/Listeners`)
+  for s in ${sock_tmp[@]};do
+    export SSH_AUTH_SOCK=$s
+    ssh-add -l >& /dev/null
+    if [ $? -eq 0 ];then
+      break
+    fi
+    unset SSH_AUTH_SOCK
+  done
+  unset sock_tmp
 fi
