48dee715bf27dee57bd5945f6aff8c1f06377e23
rcmdnk <rcmdnk@gmail.com>
2013-11-14 03:23:54 +0100

    .inputrc    .mac.sh    .vimrc    install.sh, from darwin13

--- .mac.sh
+++ .mac.sh
@@ -20,3 +20,8 @@ brew_completion=`brew --prefix 2>/dev/null`/etc/bash_completion
 if [ $? -eq 0 ] && [ -f "$brew_completion" ];then
   source $brew_completion
 fi
+
+# brew api token
+if [ -f ~/.brew_api_token ];then
+  source ~/.brew_api_token
+fi
