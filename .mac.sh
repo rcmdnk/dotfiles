bb217244e73294619af36e58bdc29fbdc5fd2dc8
rcmdnk <rcmdnk@gmail.com>
2014-04-30 05:00:01 +0900

    .mac.sh, from linux-gnu

--- .mac.sh
+++ .mac.sh
@@ -1,4 +1,46 @@
-#alias ctags='/Applications/MacVim.app/Contents/MacOS/ctags "$@"' # not in the recent MacVim
+# Use Vim in MacVim, if better
+vimversion=`vim --version`
+vimvn=`echo "$vimversion"|grep "Vi IMproved"|cut -d' ' -f 5` 2>/dev/null
+vimpatches=`echo "$vimversion"|grep "patches"|cut -d' ' -f 3|cut -d'-' -f2` 2>/dev/null
+vimluacheck=`echo "$vimversion"|grep lua|grep -v Linking|awk '{split($0,tmp,"lua")}{print substr(tmp[1],length(tmp[1]),1)}'`
+mvimversion=`mvim --version`
+mvimvn=`echo "$mvimversion"|grep "Vi IMproved"|cut -d' ' -f 5` 2>/dev/null
+mvimpatches=`echo "$mvimversion"|grep "patches"|cut -d' ' -f 3|cut -d'-' -f2` 2>/dev/null
+mvimluacheck=`echo "$mvimversion"|grep lua|grep -v Linking|awk '{split($0,tmp,"lua")}{print substr(tmp[1],length(tmp[1]),1)}'`
+
+mvimflag=0
+if [ "$mvimluacheck" = "+" -a "$vimluacheck" = "+" ];then
+  mvimflag=2
+elif [ "$mvimluacheck" = "-" -a "$vimluacheck" = "-" ];then
+  mvimflag=2
+elif [ "$mvimluacheck" = "+" -a "$vimluacheck" = "-" ];then
+  mvimflag=1
+else
+  mvimflag=0
+fi
+
+if [ $mvimflag -eq 2 ];then
+  vimret=`echo -n '$xx = '$mvimvn' <=> '$vimvn';print "$xx \n"'|perl`
+  if [ $vimret -eq 1 ];then
+    mvimflag=1
+  elif [ "$mvimpatches" = "" ];then
+    mvimflag=0
+  elif [ "$vimpatches" = "" ];then
+    mvimflag=1
+  else
+    vimret=`echo -n '$xx = '$mvimpatches' <=> '$vimpatches';print "$xx \n"'|perl`
+    if [ $vimret -eq 1 ];then
+      mvimflag=1
+    else
+      mvimflag=0
+    fi
+  fi
+fi
+
+if [ $mvimflag -eq 1 ];then
+  alias vim="mvim"
+  alias vimdiff="mvimdiff"
+fi
 
 # ssh agent
 if [ "$SSH_AUTH_SOCK" = "" ];then
@@ -20,4 +62,3 @@ brew_completion=`brew --prefix 2>/dev/null`/etc/bash_completion
 if [ $? -eq 0 ] && [ -f "$brew_completion" ];then
   source $brew_completion
 fi
-
