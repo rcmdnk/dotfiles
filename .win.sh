76611c00fe8195abfe4d476a77cef036999d1315
rcmdnk <rcmdnk@gmail.com>
2015-01-26 05:03:22 +0900

    .vimperatorrc    .win.sh, from darwin14

--- .win.sh
+++ .win.sh
@@ -37,34 +37,5 @@ alias open='cygstart'
 alias mail='email'
 #}}}
 
-# functions {{{
-# ln wrapper{{{
-if type -a busybox >& /dev/null;then
-  unalias ln >& /dev/null # for BusyBox
-fi
-function ln {
-  opt="/H"
-  if [ "$1" = "-s" ];then
-    opt=""
-    shift
-  fi
-  target="$1"
-  if [ -d "$target" ];then
-    opt="/D $opt"
-  fi
-  if [ $# -eq 2 ];then
-    link="$2"
-  elif [ $# -eq 1 ];then
-    link=`basename "$target"`
-  else
-    echo "usage: ln [-s] <target> [<link>]"
-    echo "       -s for symbolic link, otherwise make hard link"
-    return
-  fi
-  t_winpath=$(cygpath -w -a "$target")
-  t_link=$(cygpath -w -a "$link")
-  echo "cmd /c mklink $opt $t_link $t_winpath"
-  cmd /c mklink $opt "$t_link" "$t_winpath"
-}
-# }}}
-# }}}
+# Use Windows' symbolic link
+export CYGWIN="winsymlinks $CYGWIN"
