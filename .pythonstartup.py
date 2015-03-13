9c3df1a9ad7f6a5c1919be69eb60ccaf49014748
rcmdnk <rcmdnk@gmail.com>
2015-03-14 03:03:26 +0900

    .gitconfig    .pythonstartup.py    .vimrc, from darwin14

--- .pythonstartup.py
+++ .pythonstartup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-#try:
-#    import os, readline
-#    histfile = os.path.join(os.environ["HOME"], ".pyhist")
-#    readline.read_history_file(histfile)
-#    import atexit
-#    atexit.register(readline.write_history_file, histfile)
-#    del os, histfile
-#except IOError:
-#    pass
+try:
+    import os
+    import readline
+    import atexit
+    histfile = os.path.join(os.environ["HOME"], ".pyhist")
+    readline.read_history_file(histfile)
+    atexit.register(readline.write_history_file, histfile)
+except IOError:
+    pass
