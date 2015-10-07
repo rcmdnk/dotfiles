b57cb4ba4084cf461140635dd0b3cf5b345b410e
rcmdnk <rcmdnk@gmail.com>
2015-10-08 03:02:14 +0900

    .mac.sh, from darwin15

--- .mac.sh
+++ .mac.sh
@@ -108,7 +108,7 @@ fi
 # Openssl
 OPENSSL_PATH=/usr/local/opt/openssl
 if [ -d "$OPENSSL_PATH" ];then
-  export PATH=$OPENSSL_PATH/bin:$PATH
+  #export PATH=$OPENSSL_PATH/bin:$PATH
   export LD_LIBRARY_PATH=$OPENSSL_PATH/lib:$LD_LIBRARY_PATH
   export CPATH=$OPENSSL_PATH/include:$LD_LIBRARY_PATH
 fi
