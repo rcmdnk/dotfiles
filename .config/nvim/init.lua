341a6116dd19a8f20ab77e6beace55008ff904b5
rcmdnk <rcmdnk@gmail.com>
2025-02-17 15:18:28 +0900

    fix: disable clipboard integration to resolve terminal issue 28776

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,11 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ' '
 
+-- Disable clipboard integration
+-- To fix this issue, need COLORTERM=truecolor in bash env and this setting in nvim
+-- [DECRQSS +q5463;524742;73657472676266;73657472676262$qm appears in terminal · Issue #28776 · neovim/neovim](https://github.com/neovim/neovim/issues/28776)
+vim.g.clipboard = false
+
 -- Lazy plugin manager
 require("config.lazy")
 
