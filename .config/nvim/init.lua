fd56434c952931d05c1a80ccc733ee06c6d977f7
rcmdnk <rcmdnk@gmail.com>
2025-04-09 21:57:02 +0900

    refactor: extract global variable settings to config/globals.lua fix: configure python indentation global setting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,13 @@
 -- init.lua
 
 -- Following global variables must be set before loading lazy
+require("config.globals")
 
--- Set <space> as the leader key
-vim.g.mapleader = ','
-vim.g.maplocalleader = ' '
-
--- Disable clipboard integration
--- To fix this issue, need COLORTERM=truecolor in bash env and this setting in nvim
--- [DECRQSS +q5463;524742;73657472676266;73657472676262$qm appears in terminal · Issue #28776 · neovim/neovim](https://github.com/neovim/neovim/issues/28776)
-vim.g.clipboard = false
-
--- Lazy plugin manager
+-- -- Lazy plugin manager
 require("config.lazy")
 
 -- Load configurations
 require('config.options')
 require('config.keymaps')
 require('config.autocmds')
-require('config.format') 
+require('config.format')
---
+++ .config/nvim/lua/config/globals.lua
@@ -0,0 +1,13 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ','
+vim.g.maplocalleader = ' '
+
+-- Disable clipboard integration
+-- To fix this issue, need COLORTERM=truecolor in bash env and this setting in nvim
+-- [DECRQSS +q5463;524742;73657472676266;73657472676262$qm appears in terminal · Issue #28776 · neovim/neovim](https://github.com/neovim/neovim/issues/28776)
+vim.g.clipboard = false
+
+-- Fix python indentation
+vim.g.python_indent = {
+  open_paren = 'shiftwidth()',
+}
