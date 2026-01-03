0eccd5afa76405208bf9e16969816ff7baaca6a5
rcmdnk <rcmdnk@gmail.com>
2026-01-04 01:00:06 +0900

    .config/wezterm/wezterm.lua, from darwin24

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local act = wezterm.action
 
 
 -- This table will hold the configuration.
@@ -15,15 +14,18 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.default_domain = 'WSL:Ubuntu-24.04-D'
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_domain = 'WSL:Ubuntu-24.04-D'
+end
 -- config.color_scheme = 'Tokyo Night'
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 10
--- config.font = wezterm.font_with_fallback {
---    'JetBrains Mono',
---    'Cascade Mono',
--- }
+config.font_size = 12
+config.font = wezterm.font_with_fallback {
+   'Monaco',
+   'JetBrains Mono',
+   'Cascade Mono',
+}
 config.colors = {
   cursor_bg = 'silver',
   cursor_border = 'silver',
@@ -33,15 +35,15 @@ config.hide_tab_bar_if_only_one_tab  = true
 config.exit_behavior_messaging  = 'None'
 config.window_decorations = 'TITLE|RESIZE'
 config.window_padding = {
-  left = 0,
-  right = 0,
+  left = 5,
+  right = 5,
   top = 0,
   bottom = 0,
 }
 
 config.keys = {
-  { key = 'v', mods = 'ALT', action = act.PasteFrom 'Clipboard' },
-  { key = 'v', mods = 'CMD', action = act.PasteFrom 'Clipboard' },
+  { key = 'v', mods = 'ALT', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'v', mods = 'CMD', action = wezterm.action.PasteFrom 'Clipboard' },
 }
 
 -- and finally, return the configuration to wezterm
